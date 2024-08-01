049f4f335d632ea4754c0cb641506584bf1b54ae
Dssaenz <darwinsaenz105@gmail.com>
2024-08-01 17:15:33 -0500

    refactor into entity and use case

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,21 +4,31 @@ export enum LogSeverityLevel {
   high = "high",
 }
 
+interface LogEntityOptions {
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createAt?: Date;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
+  public origin: string;
   public createAt: Date;
 
-  constructor(level: LogSeverityLevel, message: string) {
+  constructor(options: LogEntityOptions) {
+    const { level, message, origin, createAt = new Date() } = options;
     this.level = level;
     this.message = message;
-    this.createAt = new Date();
+    this.createAt = createAt;
+    this.origin = origin;
   }
 
   static formJson = (json: string): LogEntity => {
-    const { message, level, createAt } = JSON.parse(json);
+    const { message, level, createAt, origin } = JSON.parse(json);
 
-    const log = new LogEntity(message, level);
+    const log = new LogEntity({ message, level, createAt, origin });
     log.createAt = new Date(createAt);
 
     return log;
--- src/domain/use-cases/check-service.ts
+++ src/domain/use-cases/check-service.ts
@@ -23,13 +23,21 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`Error on check service ${url}`);
       }
 
-      const log = new LogEntity(LogSeverityLevel.low, `Service ${url} working`);
+      const log = new LogEntity({
+        level: LogSeverityLevel.low,
+        message: `Service ${url} working`,
+        origin: "check-service.ts",
+      });
       this.logRepository.saveLog(log);
       this.successCallback();
       return true;
     } catch (error) {
       const errorMessage = `${error}`;
-      const log = new LogEntity(LogSeverityLevel.high, errorMessage);
+      const log = new LogEntity({
+        level: LogSeverityLevel.high,
+        message: errorMessage,
+        origin: "check-service.ts",
+      });
 
       this.logRepository.saveLog(log);
       this.errorCallback(errorMessage);
